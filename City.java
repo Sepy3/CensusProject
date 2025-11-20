bc8caafa7bb901ad0248c7851c3f2688a1902810
Sepy3 <negar.sepiani@usp.ceu.es>
2025-11-20 11:32:00 +0100

    Create City.java

---
+++ City.java
@@ -0,0 +1,26 @@
+public class City {
+    private String name;
+    private int population;
+
+    public City(String name, int population) {
+        this.name = name;
+        this.population = population;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    @Override
+    public String toString() {
+        return "    - City: " + name + " | Population: " + population;
+    }
+}
